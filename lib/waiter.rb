7aac9a7a232e33c11df6a963d525f3aef4edc23c
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-09-08 03:53:03 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -16,12 +16,12 @@ class Waiter
   def meals
     Meal.all.select do |meal|
     meal.customer == self
+    end
   end
   
   def best_tipper
     best_tipped_meal = meals.max do |meal_a, meal_b|
     meal_a.tip <=> meal_b.tip
+    end
   end
-  
-end
 end
